033c43001165294eb6a0c82e290dd82437334e80
Ricky Cerny <cerny.ricky@gmail.com>
2026-02-01 07:42:43 -0500

    tidied

--- api/internal/infrastructure/api/handlers/json.go
+++ api/internal/infrastructure/api/handlers/json.go
@@ -2,11 +2,16 @@ package handlers
 
 import (
 	"encoding/json"
+	"log/slog"
 	"net/http"
 )
 
 func WriteJSON(w http.ResponseWriter, status int, v any) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(status)
-	json.NewEncoder(w).Encode(v)
+	err := json.NewEncoder(w).Encode(v)
+	if err != nil {
+		slog.Error(err.Error())
+		return
+	}
 }
